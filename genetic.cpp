3c6bd17417eb55ff1569484e43749c992df862d2
Ryokusei <yuh.lopes@gmail.com>
2017-03-27 23:12:16 -0300

    Working example using new fitness function model

--- genetic.cpp
+++ genetic.cpp
@@ -8,34 +8,65 @@ using namespace std;
 #include "mutation-gaussian.h"
 #include "crossover-uniform-bitwise.h"
 
-/*
+#define ERROR_FITNESS			1e-5f
+#define BOOST_FACTOR			1e3f
+#define	PUNISHMENT_FACTOR		1e3f
+
 double fitnessFunction2(Chromosome * chromosome) {
-	//http://tutorial.math.lamar.edu/Classes/Alg/NonlinearSystems.aspx
+	//http://www.zweigmedia.com/RealWorld/simplex.html
 
 	float x = (*chromosome->getGenes())[0]->getValue().floatValue;
-	float y = (*chromosome->getGenes())[1]->getValue().floatValue;
+	uint8_t y = (*chromosome->getGenes())[1]->getValue().uint8Value;
+	//float z = (*chromosome->getGenes())[2]->getValue().floatValue;
+	//float w = (*chromosome->getGenes())[3]->getValue().floatValue;
 
 	//We have to check if the float/double values are valid
 	if (isnan(x) || isinf(x))
+		return -INFINITY;
+/*	if (isnan(y) || isinf(y))
 		return -9999;
-	if (isnan(y) || isinf(y))
+/*	if (isnan(z) || isinf(z))
 		return -9999;
-
-
-	double fitness = 10;
-	
-	if (2*x*x + y*y != 24)
-		fitness -= abs(24 - (2 * x*x + y*y));
-	
-	if (x*x - y*y != -12)
-		fitness -= abs(-12 - (x*x - y*y));
-
-	return fitness;
-}
+	if (isnan(w) || isinf(w))
+		return -9999;
 */
 
-double fitnessFunction2(Chromosome * chromosome) {
-	return (*chromosome->getGenes())[0]->getValue().uint8Value;
+	double fitness = -BOOST_FACTOR*(2 * x + y);	
+
+	bool violated = false;
+	float violations[4] = { 0, 0, 0, 0 };
+
+	/*
+	For constraint evaluation, we check if their complement is true.
+	If so, the constraint has been violated and we should punish the fitness proportionately.
+	*/
+
+	if (1.25f > x*x + (float)y) {
+		violated = true;
+		violations[0] = 1.25f - (x*x + (float)y);
+	}
+	if (x + y > 1.6f) {
+		violated = true;
+		violations[1] = 1.6f - (x+y);
+	}
+	if (x > 1.6f) {
+		violated = true;
+		violations[2] = 1.6f - x;
+	}
+
+	if (x < 0) {
+		violated = true;
+		violations[3] = -x;
+	}
+
+	if (violated) {
+		fitness = 0;
+		for (int i = 0; i <4; i++)
+			fitness -= abs(violations[i]);
+		fitness *= PUNISHMENT_FACTOR;
+	}
+
+	return fitness;
 }
 
 double fitnessFunction(Chromosome * chromosome){	
@@ -107,16 +138,13 @@ double fitnessFunction(Chromosome * chromosome){
 	return fitness;	
 }
 
-#define TOLERANCE			1e-4f
-#define OPTIMAL_FITNESS		115.0f
+#define OPTIMAL_FITNESS		2.0f
 #define MIN_SEED			0.0f
-#define MAX_SEED			40.0f
+#define MAX_SEED			1.6f
 #define POPULATION_SIZE		2000
-#define ITERATION_SHOW		50
+#define ITERATION_SHOW		1
 
-int main(){
-
-	MutationUniform m;
+int main(){	
 
 	/*
 	First we create our genotype
@@ -133,32 +161,37 @@ int main(){
 	genotype.push_back(gene);
 	*/
 
-	GeneValue minSeed, maxSeed;
+	GeneValue minSeed, maxSeed, minSeed2, maxSeed2;
 	minSeed.floatValue = MIN_SEED;
 	maxSeed.floatValue = MAX_SEED;
+
+	maxSeed2.uint8Value = 1;
+	minSeed2.uint8Value = 1;
 	
 	Gene *gene = new Gene(FLOAT);
 	gene->setSeedRange(minSeed, maxSeed);
 	genotype.push_back(gene);
 
-	gene = new Gene(FLOAT);
+	gene = new Gene(UINT8);
+	gene->setSeedRange(minSeed2, maxSeed2);
+	gene->enableBounding(true);
+	gene->setBounds(minSeed2, maxSeed2);
+	genotype.push_back(gene);
+
+	/*gene = new Gene(FLOAT);
 	gene->setSeedRange(minSeed, maxSeed);
 	genotype.push_back(gene);
 
 	gene = new Gene(FLOAT);
 	gene->setSeedRange(minSeed, maxSeed);
 	genotype.push_back(gene);
-
-	gene = new Gene(FLOAT);
-	gene->setSeedRange(minSeed, maxSeed);
-	genotype.push_back(gene);
-	
+	*/
 	
 	/*
 	Then we begin our GA setup
 	*/
 		
-	GeneticAlgorithm ga(&genotype);	
+	GeneticAlgorithm ga(&genotype);
 
 	ga.setElitism(true);	
 	ga.setEliteSize(25);
@@ -168,27 +201,23 @@ int main(){
 	ga.setCrossoverOperator(&cross);
 	ga.setCrossoverProbability(1.0f);
 	
-	vector<char> mutEnabled = { true, true, false, true };
-	MutationGaussian mut(0.0, 0.005, &mutEnabled);
+	//vector<char> mutEnabled = { true, true, false, true };
+	//MutationGaussian mut(0.0, 0.005, &mutEnabled);
+	MutationGaussian mut(0.0, 0.005);
 	ga.setMutationOperator(&mut);
 	ga.enableMutation(true);
 	ga.setMutationProbability(0.01f);	
 	
-	ga.setFitnessFunction(fitnessFunction);
+	ga.setFitnessFunction(fitnessFunction2);
 
 	ga.initializePopulation(POPULATION_SIZE);
 
 	clock_t timeBegin = clock(); //Starting time
 
 	uint64_t i;
-	for (i = 0; i < 0xFFFFFFFF; i++) {						
+	for (i = 0; i < 0xFFFFFFFF; i++) {			
 
 		ga.calculateFitness();	
-
-		if (i == 500)
-			ga.setMutationProbability(0.05f);
-		if (i == 900) 
-			ga.setMutationProbability(0.1f);					
 
 		if (i % ITERATION_SHOW == 0) {
 			cout << "Iteration " << i << endl;
@@ -196,7 +225,7 @@ int main(){
 			ga.printFittestChromosome();
 		}
 		//system("pause");
-		if (abs(ga.getFittestChromosome()->getFitness() - OPTIMAL_FITNESS) <= TOLERANCE)
+		if (abs(ga.getFittestChromosome()->getFitness() - OPTIMAL_FITNESS) <= ERROR_FITNESS)
 			break;
 
 		ga.selectionRoulette();	
@@ -209,7 +238,7 @@ int main(){
 
 	cout << "-------------------------------" << endl << endl;
 	cout << endl << "Finished at iteration " << i << endl;
-	cout << "Elapsed time: " << timeSpent << endl;
+	cout << "Elapsed time: " << timeSpent << "s" << endl;
 	cout << endl << "Final fittest chromosome: " << endl;
 	ga.calculateFitness();
 	ga.printFittestChromosome();
